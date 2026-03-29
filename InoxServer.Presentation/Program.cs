930a796f3b214d6749f26dedec2e65005b47de8e
hungpv283 <hungdz369@gmail.com>
2026-03-30 00:06:38 +0700

    feat: ci/ci deployment2

--- InoxServer.Presentation/Program.cs
+++ InoxServer.Presentation/Program.cs
@@ -40,11 +40,11 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
